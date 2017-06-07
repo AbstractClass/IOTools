fde12a3f2dd1f0eded2e9a243dd3910f54276c27
Connor <connor@lyricalsecurity.com>
2017-06-07 14:56:16 -0400

    Cool shit

--- IOTools.py
+++ IOTools.py
@@ -5,30 +5,40 @@ __version__ = "0.1"
 
 import json
 import csv
-import sys
+
+
+# Get it?  handdle-err?  I am so funny.
+def handle_err(function):
+    def handle_problem():
+        try:
+            function()
+
+        except Exception as e:
+            print("Error: ", e)
+
+            return 0
+
+    return handle_problem()
+
+
+@handle_err
+def file_io(file, permissions, io_function):
+    with open(file, permissions) as target:
+        return io_function(target)
 
 
 def json2dict(json_file):
-    try:
-        with open(json_file, 'r') as json_str:
-            a_dict = json.load(json_str)
-
-        return a_dict
-
-    except Exception as e:
-        print("Error could not open {file}: {err}".format(file=json_file, err=e))
-
-        return {"Error": e}
+    file_io(json_file, 'r', json.loads)
 
 
 def dict2json(a_dict, json_file, file_perms='w'):
-    try:
-        with open(json_file, file_perms) as file:
-            file.write(json.dumps(a_dict))
+    file_io(json_file, file_perms, lambda x: json.dump(a_dict, x))
 
-        return 1
 
-    except Exception as e:
-        print("Error could write to {file}: {err}".format(file=json_file, err=e))
+def csv2list_array(csv_file):
+    pass
 
-        return 0
+
+
+def list_array2csv(list_array, csv_file, file_perms='w'):
+    pass
